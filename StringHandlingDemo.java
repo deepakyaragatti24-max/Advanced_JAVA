c27803cbe9313a645b30c16601fc4f9693ad1ae3
deepakyaragatti24-max <deepakyaragatti24@gmail.com>
2026-04-17 17:50:26 +0530

    Add files via upload

--- StringHandlingDemo.java
+++ StringHandlingDemo.java
@@ -1,12 +1,12 @@
 /*
- * 2a. Develop a java program for performing various string operations with different
+ * 3a. Develop a java program for performing various string operations with different
 string handling functions directed as follows
 String Creation and Basic Operations, Length and Character Access, String Comparison,
 String Searching, Substring Operations , String Modification, Whitespace Handling, String
 Concatenation, String Splitting, StringBuilder Demo, String Formatting , Validate Email with
 contains( ) and startsWith() and endsWith()
  */
-package String_Functions;
+package lab3;
 public class StringHandlingDemo {
     public static void main(String[] args) {
         // Basic String Creation
